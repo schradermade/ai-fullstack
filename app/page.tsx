0f11e66ffd6c9e65974de8a01d244e1fd7ce79f4
Nathan Schrader <natecschrader@gmail.com>
2023-10-02 09:16:51 -0700

    small CSS changes to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px] mx-auto ">
+    <div className="w-screen h-screen bg-black flex items-center text-white">
+      <div className="max-w-[500px] mx-auto ">
         <h1 className="text-6xl mb-4">Journal App</h1>
         <p className="text-2xl text-white/60 mb-4">
-          Track your mood over time!
+          Write in your journal and let AI analyze and track your mood over
+          time!
         </p>
         <div>
-          <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
-            get started
-          </button>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
         </div>
       </div>
     </div>
