53d8fecfb432ab19b9e23fe37911af20454be066
Nathan Schrader <natecschrader@gmail.com>
2023-10-02 11:28:26 -0700

    add middleware for Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
-import { Clerk } from '@clerk/nextjs/server'
 
 const inter = Inter({ subsets: ['latin'] })
 
