87371281c39fb63ca0ac23020260913445184a97
Nathan Schrader <natecschrader@gmail.com>
2023-10-02 14:29:45 -0700

    add logic to fetch journal entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,22 @@
-const JournalPage = () => {
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntries = async () => {
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return entries
+}
+
+const JournalPage = async () => {
+  const entries = await getEntries()
+  console.log('ENTIRES:', entries)
   return <div>Journal</div>
 }
 
