111fc84b967e3415b4c22b274408d567f87d1cfd
Nathan Schrader <natecschrader@gmail.com>
2023-10-02 19:17:12 -0700

    add logic to add new entry card

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { createNewEntry } from '@/utils/api'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 const NewEntryCard = () => {
+  const router = useRouter()
+
+  const handleOnClick = async () => {
+    const data = await createNewEntry()
+    router.push(`/journal/${data.id}`)
+  }
+
   return (
-    <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
-      <div className="px-4 py-5 sm:p-6">
-        <span className="text-3xl">New Entry</span>
+    <Link href={'/'}>
+      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+        <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
+          <span className="text-3xl">New Entry</span>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
