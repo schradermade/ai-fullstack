8062e58ebf69bb0c8e2d2a76d26832210ff01e58
Nathan Schrader <natecschrader@gmail.com>
2023-10-02 19:51:53 -0700

    add height full css prop

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -14,7 +14,7 @@ const NewEntryCard = () => {
 
   return (
     <Link href={'/'}>
-      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+      <div className="h-full cursor-pointer overflow-hidden rounded-lg bg-white shadow">
         <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
           <span className="text-3xl">New Entry</span>
         </div>
