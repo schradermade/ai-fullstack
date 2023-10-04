a76b8580c9234b33ad266af95bc8e23b6d12a598
Nathan Schrader <natecschrader@gmail.com>
2023-10-04 09:38:40 -0700

    add and configure autosave in Editor

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,25 @@ const createURL = (path) => {
   return window.location.origin + path
 }
 
+export const updateEntry = async (id, content) => {
+  const res = await fetch(new Request(createURL(`/api/journal/${id}`)), {
+    method: 'PATCH',
+    body: JSON.stringify({ content }),
+  })
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+
+  // When error..
+  return {
+    error: true,
+    code: 2323,
+    message: 'Something went wrong updating entry!',
+  }
+}
+
 export const createNewEntry = async () => {
   const res = await fetch(new Request(createURL('/api/journal')), {
     method: 'POST',
