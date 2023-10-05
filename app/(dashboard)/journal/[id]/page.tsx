901647d11480c1dd436b3de04c37bd086c5ec91b
Nathan Schrader <natecschrader@gmail.com>
2023-10-05 11:38:38 -0700

    wire up Analysis values to UI

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Editor from '@/components/Editor'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import { useEffect, useState } from 'react'
 
 const getEntry = async (id) => {
   const user = await getUserByClerkId()
@@ -11,15 +12,51 @@ const getEntry = async (id) => {
         id,
       },
     },
+    include: {
+      analysis: true,
+    },
   })
   return entry
 }
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const { summary, subject, mood, color, negative } = entry?.analysis
+    ? entry.analysis
+    : {}
+
+  console.log('BOOL:', negative)
+
+  const analysisData = [
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative.toString() },
+  ]
   return (
-    <div className="h-full w-full">
-      <Editor entry={entry} />
+    <div className="h-full w-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+
+        <div>
+          <ul>
+            {analysisData.map((item, i) => (
+              <li
+                key={i}
+                className=" px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
