5583de4f2567d00264368c57e45bce743491f1a5
Nathan Schrader <natecschrader@gmail.com>
2023-10-05 12:38:21 -0700

    moved all UI content into Editor component

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -5,26 +5,58 @@ import { useState } from 'react'
 import { useAutosave } from 'react-autosave'
 
 const Editor = ({ entry }) => {
+  const [analysis, setAnalysis] = useState(entry.analysis)
   const [value, setValue] = useState(entry.content)
   const [isLoading, setIsLoading] = useState(false)
+
+  const { summary, subject, mood, color, negative } = analysis
+  const analysisData = [
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Negative', value: negative ? 'True' : 'False' },
+  ]
 
   useAutosave({
     data: value,
     onSave: async (_value) => {
       setIsLoading(true)
-      const updated = await updateEntry(entry.id, _value)
+      const data = await updateEntry(entry.id, _value)
+      setAnalysis(data.analysis)
       setIsLoading(false)
     },
   })
 
   return (
-    <div className="h-full">
-      {isLoading && <div>...Loading</div>}
-      <textarea
-        className="h-[calc(100vh-67px)] w-full p-8 text-xl outline-none"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+    <div className="h-full grid grid-cols-3">
+      <div className="col-span-2">
+        {isLoading && <div>...Loading</div>}
+        <textarea
+          className="h-[calc(100vh-67px)] w-full p-8 text-xl outline-none"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+
+      <div className="border-l border-black/10">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+
+        <div>
+          <ul>
+            {analysisData.map((item, i) => (
+              <li
+                key={i}
+                className=" px-2 py-4 flex items-center justify-between border-b border-t border-black/10"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
+                <span>{item.value}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
